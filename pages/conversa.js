b7b7f421793b073ebe6c2c50fb969b57a7871b47
MassayoshiDev <psnnapoleandro@gmail.com>
2025-04-30 12:56:46 -0300

    test

--- pages/conversa.js
+++ pages/conversa.js
@@ -1,3 +1,13 @@
 function goBackToHomePage() {
     window.location.replace("../index.html")
 }
+
+const op = document.getElementById("options")
+op.style.opacity = 0
+function showOptions() {
+    if (op.style.opacity = 1) {
+        op.style.opacity = 0
+    } else {
+        op.style.opacity = 1
+    }
+}
