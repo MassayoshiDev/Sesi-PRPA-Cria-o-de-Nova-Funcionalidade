4d402045fb46875b3d3f7db322fa4ec151e8e961
MassayoshiDev <psnnapoleandro@gmail.com>
2025-05-05 10:57:55 -0300

    trabalho terminado

--- pages/conversa.js
+++ pages/conversa.js
@@ -5,9 +5,19 @@ function goBackToHomePage() {
 const op = document.getElementById("options")
 op.style.opacity = 0
 function showOptions() {
-    if (op.style.opacity = 1) {
+    if (op.style.opacity == 1) {
         op.style.opacity = 0
+        op.style.zIndex = 1
     } else {
         op.style.opacity = 1
+        op.style.zIndex = 2
     }
+}
+
+function traduzirBatePapo() {
+    window.location.replace("conversaTraduzida.html")
+}
+
+function DesligarTradutor() {
+    window.location.replace("conversa.html")
 }
